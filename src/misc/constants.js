17c15f0057835b545c3df1ab6f5e6f7a39114cc3
Athi <athisankar@mcci.com>
2024-01-30 11:06:03 +0530

    Updated DNC V2code

---
+++ src/misc/constants.js
@@ -0,0 +1,9 @@
+export const constobj = {
+    SW_APP: 'DNC UI',
+    SW_VER: '2.0.0-1',
+    // DNC_URL: 'http://localhost:8891',
+    DNC_URL: 'https://training.mcci.io/dncserver',
+    // DNC_URL: 'https://www.cornellsaprun.com/dncserverv2/',
+    // DNC_URL: 'https://iotdashboard.mcci.io/dncserver',
+    CPLUGIN_URL: 'https://training.mcci.io/dncservertest'
+};
